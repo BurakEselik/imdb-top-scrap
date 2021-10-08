d2219ae13899d573d0c32f10f04df1ceabedc180
BurakEselik <eselik.burak@gmail.com>
2021-10-08 22:37:21 +0300

    modified

--- imdb_top_scrap.py
+++ imdb_top_scrap.py
@@ -1,28 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+movie_list = []
+top_movies_list = list()
 
 url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
-
 result = requests.get(url)
 soup = BeautifulSoup(result.text, 'html.parser')
 
-t_body = soup.tbody
+def extract_imdb_data():
+    global movie_list
+    global top_movies_list
+    t_body = soup.tbody
+    t_rs = t_body.find_all('tr')
 
-t_rs = t_body.find_all('tr')
-movie_list = []
-top_movies_list = list()
-for tr in t_rs:
-    t_ds = tr.find_all('td')
-    movie_datas = str(t_ds[1].get_text()).strip() + str(t_ds[2].get_text()).strip()
-    movie_list.append(movie_datas)
+    for tr in t_rs:
+        t_ds = tr.find_all('td')
+        movie_datas = str(t_ds[1].get_text()).strip() + str(t_ds[2].get_text()).strip()
+        movie_list.append(movie_datas)
 
-#print(movie_list)
-for movie in movie_list:
-    number, name, date = movie.split('\n')
-    top_movies_list.append([
-        name.strip(),
-        date[1:5],
-        date[-3:]
-    ])
+    #print(movie_list)
+    for movie in movie_list:
+        number, name, date = movie.split('\n')
+        top_movies_list.append([
+            int(number[:-1]),
+            name.strip(),
+            date[1:5],
+            date[-3:]
+        ])
 
-print(top_movies_list)
+
+def convert_csv():
+    
+    header_list = ["NUMBER", "MOVIE NAME", "YEAR", "RATE"]
+    rows = top_movies_list
+
+    #name of csv file
+    file_name = 'top_250_movies.csv'
+
+    with open(file_name, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header_list)
+        for row in rows:
+            csvwriter.writerow(row)
+
+
+def main():
+    extract_imdb_data()
+    convert_csv()
+
+
+if __name__ == '__main__':
+    main()
+    
